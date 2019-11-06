779f4420bb6ec2d676682f4c6d48125a94a3672e
woony91 <49602639+woony91@users.noreply.github.com>
2019-11-05 23:50:10 -0500

    got trashcan to work and included the plus sign

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,6 @@ $('input[type="text"]').keypress(function(event){
         let todoText = $(this).val();
         $(this).val('');
         //create a new li and add to ul
-        $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+        $('ul').append('<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ' + todoText + '</li>');
     }
 });
