64480b942731fb2f757db1870c41789c80978317
woony91 <49602639+woony91@users.noreply.github.com>
2019-11-05 23:09:11 -0500

    Allowing text input to add to the todo list

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,23 @@
 //Check off specific todos by clicking
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
     $(this).toggleClass('completed');
 });
    
+//Click on X to delete todo
+$('ul').on('click', 'span', function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//Hit Enter to submit new todo onto list
+$('input[type="text"]').keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo text from input
+        let todoText = $(this).val();
+        $(this).val('');
+        //create a new li and add to ul
+        $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+    }
+});
